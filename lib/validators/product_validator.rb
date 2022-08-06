6f535bc7ddc2ac8358fbc45bb749023c9018256f
pulpecja <pulpecja@gmail.com>
2022-08-06 19:33:42 +0200

    add valid? code for product_validator

--- lib/validators/product_validator.rb
+++ lib/validators/product_validator.rb
@@ -3,6 +3,34 @@
 module Validators
   class ProductValidator
     def valid?(product)
+      required_attributes_present?(product) &&
+        valid_price?(product.price)
+    end
+
+    private
+
+    def required_attributes_present?(product)
+      required_attributes.each do |attr|
+        return false if product.send(attr).nil?
+      end
+
+      true
+    end
+
+    def required_attributes
+      %i[code name price]
+    end
+
+    def valid_price?(price)
+      price_float?(price) && price_positive?(price)
+    end
+
+    def price_float?(price)
+      price.is_a? Float
+    end
+
+    def price_positive?(price)
+      price > 0
     end
   end
 end
